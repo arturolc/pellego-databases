bef93e0bbb1f4f850104520368c46f32a7725b35
u1215936 <u1215936@utah.edu>
2021-03-29 15:06:28 -0600

    Added the LM_Quiz table back. Didn't realize that was mostly tied to scores. Fixed a mysql error in Questions table.

--- database_scripts/create_tables.sql
+++ database_scripts/create_tables.sql
@@ -35,6 +35,13 @@ Primary Key (SMID),
 Foreign Key (MID) References LM_Module(MID)
 );
 
+CREATE TABLE LM_Quiz (
+QID int AUTO_INCREMENT,
+SMID int,
+Primary Key (QID),
+Foreign Key (SMID) References LM_Submodule(SMID)
+);
+
 CREATE TABLE Questions
 (
 QUID int AUTO_INCREMENT,
